4b476977203a7e045b8da1f8d2ed62883caf57f6
Reyofel Golecruz <refgolecruz.nz@gmail.com>
2026-06-08 02:50:28 +0000

    optimized models

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,57 +1,29 @@
 with customers as (
-
-select * from {{ ref('stg_jaffle_shop__customers') }}
-
+    select * from {{ ref ('stg_jaffle_shop__customers')}}
 ),
-
 orders as (
-
- select * from {{ ref('stg_jaffle_shop__orders') }}
-
+    select * from {{ ref ('fct_orders')}}
 ),
-
 customer_orders as (
-
     select
         customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        min (order_date) as first_order_date,
+        max (order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
-
 ),
-
-total_orders as (
-select
-    customer_id,
-    sum(amount)/100 as total_value
-
-from {{ ref('fct_orders') }}
-
-group by customer_id
-)
-,
-
-final as (
-
+ final as (
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        total_orders.total_value as lifetime_value
-
+        coalesce (customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-    left join total_orders using(customer_id)
-
 )
-
 select * from final
